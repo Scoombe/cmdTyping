df19c2537ff5872196555e230516e4776ea70aa0
Matt Wilson <matt@matt-wilson.org>
2018-10-07 23:05:38 +0100

    Apply linting fixes to test file

--- test/typingTs.test.ts
+++ test/typingTs.test.ts
@@ -1,23 +1,23 @@
 import {expect} from 'chai';
 import {cmdTyping} from '../src/typing';
 
-let chalk = require("chalk");
-let typeTest = new cmdTyping(printText, function(){console.log("finish has run");},0.01, true);
+const chalk = require('chalk');
+const typeTest = new cmdTyping(printText, function() {console.log('finish has run'); }, 0.01, true);
 /**
  * @function to print the displayString text to the console.
  */
-function printText() : void {
-    
+function printText(): void {
+
 }
 
-describe ( "typing.ts tests" , function(){
-    typeTest.wordsTest.curDisplayText = "aaaaaaaaaaaa"
-    it ( "should have the right display text ", function() {
-        typeTest.getDisplayText(false)
-        let text = getTestDisplayText("aaaaaaaaaaaa",0,0,0);
+describe ( 'typing.ts tests' , function() {
+    typeTest.wordsTest.curDisplayText = 'aaaaaaaaaaaa';
+    it ( 'should have the right display text ', function() {
+        typeTest.getDisplayText(false);
+        const text = getTestDisplayText('aaaaaaaaaaaa', 0, 0, 0);
         expect(typeTest.displayString).to.equal(text);
     });
-    it ( "should have the right finish text ", function() {
+    it ( 'should have the right finish text ', function() {
         typeTest.getFinishText();
         expect(typeTest.displayString).to.equal(`${chalk.red(`you have finished, press control r to go again. `)}
 Words written: 0
@@ -30,32 +30,32 @@ From: ${chalk.green(``)}
 WPM:  ${chalk.green(`0`)}
 AverageWPM: ${chalk.green(`0`)}`);
     });
-    it ("Should check key chars correctly ", function() {
+    it ('Should check key chars correctly ', function() {
         typeTest.wordsTest.started = true;
-        typeTest.wordsTest.completeText = "aaaaaaaaaaaa";
-        typeTest.checkKey("b");
-        expect(typeTest.displayString).to.equal(getTestDisplayText("aaaaaaaaaaaa",0,0,0,"a"));
-        typeTest.checkKey("a");
-        expect(typeTest.displayString).to.equal(getTestDisplayText("aaaaaaaaaaa",0,0,0,));
-        typeTest.checkKey("a");
-        expect(typeTest.displayString).to.equal(getTestDisplayText("aaaaaaaaaa",0,0,0));
-        typeTest.checkKey("a");
-        expect(typeTest.displayString).to.equal(getTestDisplayText("aaaaaaaaa",0,0,0));
-        typeTest.checkKey("b");
-        expect(typeTest.displayString).to.equal(getTestDisplayText("aaaaaaaaa",0,0,0,"a"));
+        typeTest.wordsTest.completeText = 'aaaaaaaaaaaa';
+        typeTest.checkKey('b');
+        expect(typeTest.displayString).to.equal(getTestDisplayText('aaaaaaaaaaaa', 0, 0, 0, 'a'));
+        typeTest.checkKey('a');
+        expect(typeTest.displayString).to.equal(getTestDisplayText('aaaaaaaaaaa', 0, 0, 0, ));
+        typeTest.checkKey('a');
+        expect(typeTest.displayString).to.equal(getTestDisplayText('aaaaaaaaaa', 0, 0, 0));
+        typeTest.checkKey('a');
+        expect(typeTest.displayString).to.equal(getTestDisplayText('aaaaaaaaa', 0, 0, 0));
+        typeTest.checkKey('b');
+        expect(typeTest.displayString).to.equal(getTestDisplayText('aaaaaaaaa', 0, 0, 0, 'a'));
 
     });
 });
 
-function getTestDisplayText(displayString: string, wordsTyping: number, AverageWPM: number, totalAverageWPM: number, errorText?: string,) : string{
+function getTestDisplayText(displayString: string, wordsTyping: number, AverageWPM: number, totalAverageWPM: number, errorText?: string, ): string {
     let text = ``;
-    if(errorText){
+    if (errorText) {
         text = `${chalk.red(errorText)}
 `;
     }
     text += `${displayString}
 words typed: ${chalk.green(`${wordsTyping}`)}
 word average WPM: ${chalk.green(`${AverageWPM}`)}
-total average WPM: ${chalk.green(`${totalAverageWPM}`)}`
-return text;
+total average WPM: ${chalk.green(`${totalAverageWPM}`)}`;
+    return text;
 }
